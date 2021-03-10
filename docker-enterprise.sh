cc21f36005ded6144cc5c5b2451e821babb61efe
Web Publisher <graalvm-dev@oss.oracle.com>
2021-03-10 15:14:25 +0000

    Update website

--- docker-enterprise.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -ex
-DOCKER=$(which docker 2>/dev/null || which podman)
-$DOCKER build . -f Dockerfile.enterprise -t graalvm.org:enterprise
-$DOCKER run --privileged --rm -it -v $(pwd):/graalvm.org -p 4000:4000 graalvm.org:enterprise
