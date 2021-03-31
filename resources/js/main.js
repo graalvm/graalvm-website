4bc2be50678751efc1d91e340f607dd4a726852c
Web Publisher <graalvm-dev@oss.oracle.com>
2021-03-31 15:33:58 +0000

    Update website

--- resources/js/main.js
+++ resources/js/main.js
@@ -1226,3 +1226,42 @@ $("a").each(function() {
         $(this).addClass('activeMenuItem');
     }
 });
+
+
+// Automatic slideshow with manual navigation for announcements banner
+var slideIndex = 0;
+var slides = document.getElementsByClassName("adSlides");
+var dots = document.getElementsByClassName("dot");
+
+showSlides();
+
+function showSlides() {
+   var i;
+   for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";
+   }
+   slideIndex++;
+   if (slideIndex> slides.length) {slideIndex = 1}
+   for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" active", "");
+   }
+   slides[slideIndex-1].style.display = "block";
+   dots[slideIndex-1].className += " active";
+   setTimeout(showSlides, 6000); // Change image every 6 seconds
+}
+
+function currentSlide(no) {
+   var i;
+   for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";
+   }
+   slideIndex = no;
+   slides[no-1].style.display = "block";
+}
+
+function plusSlides(n) {
+ var newslideIndex = slideIndex + n;
+ if(newslideIndex < 4 && newslideIndex > 0){
+    currentSlide(newslideIndex);
+ }
+}
