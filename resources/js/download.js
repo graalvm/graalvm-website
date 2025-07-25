ab720f1efab5634979edf64405ae07bb9acb6bc6
Web Publisher <graalvm-dev@oss.oracle.com>
2025-07-25 11:53:30 +0000

    Update website

--- resources/js/download.js
+++ resources/js/download.js
@@ -88,8 +88,8 @@ const platforms = {
 
 const fullJavaVersions = {
   "17": "17.0.12",
-  "21": "21.0.7",
-  "24": "24.0.1",
+  "21": "21.0.8",
+  "24": "24.0.2",
 }
 
 function updateGHASnippet(majorJavaVersion) {
@@ -110,7 +110,7 @@ docker pull container-registry.oracle.com/graalvm/jdk:${majorJavaVersion}`);
 
 function updateSDKMANSnippet(majorJavaVersion) {
   const fullJavaVersion = fullJavaVersions[majorJavaVersion];
-  $("#dl-snippet-sdkman").text(`sdk install java ${fullJavaVersion}-graal`);
+  $("#dl-snippet-sdkman").text(`sdk install java ${fullJavaVersion}-graal  # coming soon`);
 }
 
 function updateScriptFriendlyURLsSnippet(majorJavaVersion, platform, fileExtension) {
